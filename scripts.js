0866461582310bf3b38d5e6c7f4af91a1ebdfcfa
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2020-04-27 20:44:44 -0300

    Finalização temporária do prjeto, mais função serão implementadas no futuro
    A aplicação está por hora pronta com os layouts e interações JS já realizadas. Projeto sujeito a mudanças no futuro para implementações de novas funcionalidades

--- scripts.js
+++ scripts.js
@@ -1,12 +1,25 @@
+const recipes = document.querySelectorAll('.recipe');
+
 const modalFull = document.querySelector('.modal_full');
-const recipes = document.querySelectorAll('.recipe')
+const modal = modalFull.querySelector('.modal')
+const contents = modal.querySelectorAll('.modal_content')
 
 for (let recipe of recipes) {
-    recipe.addEventListener("click", function apear () {
-        const courseId = recipe.getAttribute('id');
+    recipe.addEventListener("click", function () {
         modalFull.classList.add('active')
+        
+        const courseId = recipe.getAttribute('id');
+        modal.querySelector('img').src = `assets/${courseId}.png`;
 
-        modalFull.querySelector('.modal').id = courseId
+        for (let content of contents) {
+            let contentId = content.getAttribute('id')
+
+            if (contentId === courseId) {
+                content.classList.add('active')
+            } else {
+                content.classList.remove('active')
+            }
+        } 
     })
 }
 
