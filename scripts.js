12139686cb3b00e6faf2001d02685a4b768684c0
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2020-04-28 17:05:02 -0300

    Finalização temporaria do projeto
    O projeto Foodfy está finalizado por hora, com todos seus layouts e funcionalidades realizados. No futuro o projeto será atualizado para adicião de novos conteúdo e funcionalidades

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ const modalFull = document.querySelector('.modal_full');
 const modal = modalFull.querySelector('.modal')
 const contents = modal.querySelectorAll('.modal_content')
 
+const maximized = document.querySelector('.maximized')
+
 for (let recipe of recipes) {
     recipe.addEventListener("click", function () {
         modalFull.classList.add('active')
@@ -20,9 +22,21 @@ for (let recipe of recipes) {
                 content.classList.remove('active')
             }
         } 
+            modalFull.querySelector('.expand_modal').addEventListener("click", function () {
+                maximized.classList.add('maximize')
+                modal.classList.add('none')
+        
+            maximized.querySelector('img').src = `assets/${courseId}.png`;
+        })
     })
+
 }
 
 modalFull.querySelector('.close_modal').addEventListener("click", function () {
     modalFull.classList.remove('active')
+})
+
+maximized.querySelector('.expand_modal').addEventListener("click", function () {
+    maximized.classList.remove('maximize')
+    modal.classList.remove('none')
 })
